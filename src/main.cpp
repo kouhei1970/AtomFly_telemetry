bfca0d5112d6f94d098039cba789abf446025dd4
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2024-02-11 22:20:11 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,13 @@
 #include <M5Atom.h>
 #include <WiFi.h>
 #include <esp_now.h>
+#include <esp_wifi.h>
 
+#define CHANNEL 1
+
+uint8_t Channel = CHANNEL;
 uint8_t data[1];
+esp_now_peer_info_t peerInfo;
 
 void dataRecv(const uint8_t *addr, const uint8_t *data, int datasize) {
   //データ受信時に実行したい内容をここに書く。
@@ -28,6 +33,16 @@ void dataRecv(const uint8_t *addr, const uint8_t *data, int datasize) {
   Serial.printf("\r\n");
 }
 
+void change_channel(uint8_t ch)
+{
+  peerInfo.channel = ch;
+  if (esp_now_mod_peer(&peerInfo)!=ESP_OK)
+  {
+        Serial.println("Failed to modify peer");
+        return;
+  }
+  esp_wifi_set_channel(ch, WIFI_SECOND_CHAN_NONE);
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -37,12 +52,32 @@ void setup() {
   WiFi.disconnect(); //初期化前にWi-Fi接続を切断
   Serial.printf("MAC ADDRESS: %s\r\n", (WiFi.macAddress()).c_str());
   if (esp_now_init() == ESP_OK) {
-    Serial.println("ESP-Now Init Success");
+    Serial.println("ESPNow Init Success");
+  } else {
+    Serial.println("ESPNow Init Failed");
+    ESP.restart();
+  }
+  //ペアリング
+  peerInfo.channel = CHANNEL;
+  peerInfo.encrypt = false;
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) 
+  {
+        Serial.println("Failed to add peer");
+        return;
   }
   esp_now_register_recv_cb(dataRecv);
+  esp_wifi_set_channel(CHANNEL, WIFI_SECOND_CHAN_NONE);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while(1);
+  M5.update();
+  if (M5.Btn.wasPressed())
+  {
+    Channel++;
+    if (Channel==15)Channel=1;
+    change_channel(Channel);
+    Serial.printf("Channel %02d\n\r", Channel);
+  }
 }
